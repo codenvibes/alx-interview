68a13bbd69c9d890413e7a68ddf7ebef4a507321
codenvibes <terrymk1999@gmail.com>
2024-07-15 17:11:56 +0300

    Feat: Implement N-Queens solver

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -15,78 +15,90 @@ Arguments:
 
 import sys
 
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
 
-try:
-    n_q = int(sys.argv[1])
-except ValueError:
-    print('N must ba a number')
-    exit(1)
+def is_valid_position(board, row, col):
+    """check for validity of the queens position
+    """
+    b_size = len(board)
+    if sum(board[row]) or sum([board[i][col] for i in range(b_size)]) != 0:
+        return False
 
-if n_q < 4:
-    print('N must ba at least 4')
-    exit(1)
-
-
-def solve_nqueens(n):
-    '''
-    Solve the N-Queens problem.
-
-    Args:
-        n (int): The number of queens and the size of the chessboard (NxN).
-
-    Returns:
-        List[List[Tuple[int, int]]]: A list of solutions, where each solution
-        is a list of tuples representing the positions of queens on the
-        chessboard.
-    '''
-    if n == 0:
-        return [[]]
-    inner_solution = solve_nqueens(n - 1)
-    return [solution + [(n, i + 1)]
-            for i in range(n_q)
-            for solution in inner_solution
-            if safe_queen((n, i + 1), solution)]
-
-
-def attack_queen(square, queen):
-    '''
-    Determine if a queen can attack another queen.
-
-    Args:
-        square (Tuple[int, int]): The position of the first queen (row, column)
-        queen (Tuple[int, int]): The position of the second queen (row, column)
-
-    Returns:
-        bool: True if the queens can attack each other, False otherwise.
-    '''
-    (row1, col1) = square
-    (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or\
-        abs(row1 - row2) == abs(col1 - col2)
-
-
-def safe_queen(sqr, queens):
-    '''
-    Check if a queen can be placed on the board without being attacked.
-
-    Args:
-        sqr (Tuple[int, int]): The position to check (row, column).
-        queens (List[Tuple[int, int]]): The positions of the existing queens.
-
-    Returns:
-        bool: True if the position is safe, False otherwise.
-    '''
-    for queen in queens:
-        if attack_queen(sqr, queen):
-            return False
+    for i, j in [(1, 1), (-1, -1), (1, -1), (-1, 1)]:
+        r, c = row, col
+        while 0 <= r + i < b_size and 0 <= c + j < b_size:
+            r, c = r + i, c + j
+            if board[r][c]:
+                return False
     return True
 
 
-for answer in reversed(solve_nqueens(n_q)):
-    result = []
-    for p in [list(p) for p in answer]:
-        result.append([i - 1 for i in p])
-    print(result)
+def put_next_queen(board, row):
+    """Makes sure a queen is placed at a valid position
+    """
+    st, end = 0, len(board)
+    if sum(board[row]) == 1:
+        st = board[row].index(1) + 1
+        board[row] = [0 for col in range(end)]
+
+    for col in range(st, end):
+        if is_valid_position(board, row, col):
+            board[row][col] = 1
+            return True
+    return False
+
+
+def find_nqueens_solution(board, solutions=[]):
+    """Solves the nqueens problem
+
+    Args:
+        n (int): size of board
+    """
+    n = len(board)
+    row = 0
+    while row < n:
+        if put_next_queen(board, row):
+            row += 1
+        else:
+            if row - 1 < 0:
+                break
+            row -= 1
+        if row == n:
+            solutions.append([[row, board[row].index(1)] for row in range(n)])
+            row -= 1
+
+    if row == 0:
+        return
+
+    solutions.append([[row, board[row].index(1)] for row in range(n)])
+    idx = board[0].index(1)
+    if idx > -1:
+        board = [[0 for _ in range(n)] for row in range(n)]
+        board[0][idx] = 1
+        find_nqueens_solution(board, solutions)
+
+
+def find_solutions(n):
+    """Find each solution and prints them
+    """
+    board = [[0 for col in range(n)] for row in range(n)]
+    solutions = []
+    find_nqueens_solution(board, solutions)
+    for row in solutions:
+        print(row)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+        if n < 4:
+            print('N must be at least 4')
+            sys.exit(1)
+        find_solutions(n)
+
+    except ValueError:
+        print('N must be a number')
+        sys.exit(1)
